a745c3c584888f4c6e3f244cef918b0f4783674c
yahia-kplr <yahia.maamar@kplr.fr>
2023-03-03 11:40:57 +0100

    unicode resolv

--- generator/fonc_utils.py
+++ generator/fonc_utils.py
@@ -1,21 +1,26 @@
+# coding: utf-8
 import re
 import json
+from unidecode import *
 
 def write_content(content,filename):
-        with open(filename, "w") as f:
+        with open(filename, "w", encoding='utf-8') as f:
             f.write(content)
             
 
 def read_json(filename: str = "json_data.json"):
-    return json.load(open(filename, "r", encoding='utf-8'))
+    
+    return json.load(open(filename, "rb"))
             
             
 def sep():
     print("====================\n")
     
     
-def trimspaces(input):
+def trimspaces(data):
     # Define a regular expression pattern to match quoted substrings
     pattern = r'"[^"]*"'
     # Replace spaces and hyphens with underscore
-    return re.sub(pattern, lambda m: m.group(0).replace(" ", "_").replace("-", "_"), json.dumps(input))
+    #return re.sub(pattern, lambda m: m.group(0).replace(" ", "_").replace("-", "_"), str(unidecode(json.dumps(data))))
+    data_s=json.dumps(data)
+    return re.sub(pattern, lambda m: m.group(0).replace(" ", "_").replace("-", "_"), data_s)
